b291fb7fbf6c52bb7ad1f9ca0e93c28e2d0d6416
theoallen <theoallen@users.noreply.github.com>
2021-12-15 06:58:09 +0700

    Create Theo_AnimationControl_MZ.js

---
+++ MIT/Theo_AnimationControl_MZ.js
@@ -0,0 +1,98 @@
+/*:
+@target MZ
+@plugindesc v1.0.0 - Animation FPS and Offset Control
+@url https://github.com/theoallen/RMMZ
+@author TheoAllen
+@help
+♦ What it does?
+Set the frame rate for each MV-Style animation
+Set the offset X and Y for each MV-Style animation
+
+♦ How to use:
+Use <rate: x> in the animation name to determine the rate. 
+<rate: 1> = 60 FPS
+<rate: 2> = 30 FPS
+<rate: 3> = 20 FPS (This plugin default)
+<rate: 4> = 15 FPS (Default RPG Maker)
+
+Use <x: n> and <y: n> to determine the central offset.
+
+♦ Terms of Use:
+MIT
+
+@param defrate
+@type number
+@min 1
+@default 3
+@text Default Rate
+@desc Default frame rate for the animation
+*/
+
+// Screen Default
+// 816
+// 624
+
+Theo = Theo || {}
+Theo.AnimControl = function(){
+    const $ = Theo.AnimControl
+    $._pluginName = document.currentScript.src.match(/.+\/(.+)\.js/)[1]
+    $._version = '1.0.0'
+
+    $._params = PluginManager.parameters($._pluginName)
+    $._defaultRate = Number($._params.defrate)
+
+    $._tagRate = /<rate\s*:\s*(\d+)\s*>/i
+    $._tagOffsetX = /<x:(-|\+*)(\d+)>/
+    $._tagOffsetY = /<y:(-|\+*)(\d+)>/i
+
+    $.getFrameRate = function(anim){
+        if(!anim._rate){
+            anim._rate = $._defaultRate
+            const match = anim.name.match(Theo.AnimControl._tagRate)
+            if(match){
+                anim._rate = Number(match[1])
+            }
+        }
+        return anim._rate
+    }
+
+    $.getOffsetX = function(anim){
+        if(!anim._offsetX){
+            anim._offsetX = 0
+            const match = anim.name.match(Theo.AnimControl._tagOffsetX)
+            if(match){
+                anim._offsetX = Number(match[2]) * (String(match[1]) === "-" ? -1 : 1)
+            }
+        }
+        return anim._offsetX
+    }
+
+    $.getOffsetY = function(anim){
+        if(!anim._offsetY){
+            anim._offsetY = 0
+            const match = anim.name.match(Theo.AnimControl._tagOffsetY)
+            if(match){
+                anim._offsetY = Number(match[2]) * (String(match[1]) === "-" ? -1 : 1)
+            }
+        }
+        return anim._offsetY
+    }
+
+    // Overwrite setup rate
+    Sprite_AnimationMV.prototype.setupRate = function() {
+        this._rate = Theo.AnimControl.getFrameRate(this._animation);
+    };
+
+    $.updatePosAlias = Sprite_AnimationMV.prototype.updatePosition
+    Sprite_AnimationMV.prototype.updatePosition = function() {
+        Theo.AnimControl.updatePosAlias.call(this)
+        // just in case
+        if(this._animation){
+            x = Theo.AnimControl.getOffsetX(this._animation)
+            console.log(x)
+            this.x -= Theo.AnimControl.getOffsetX(this._animation)
+            this.y -= Theo.AnimControl.getOffsetY(this._animation)
+        }
+    };
+}
+Theo.AnimControl()
